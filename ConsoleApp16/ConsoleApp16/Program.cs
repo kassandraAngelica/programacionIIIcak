130fe2b7cd5cda4ddd85526d8cf9273b8c3c730e
kassandraAngelica <kassandraangelica1234@gmail.com>
2020-03-23 13:50:08 -0300

    Tarea 6. fibonacci Recursivo

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+            Console.WriteLine("introducir posicion");
+            int n = int.Parse(Console.ReadLine());
+            while (numero <= n)
+            {
+                Console.Write(fibonacci(numero)+" ");
+                numero++;
+                
+            }
+            Console.ReadKey();
+        }
+      
+        public static int fibonacci(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            else
+                return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+    
+}
